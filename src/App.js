f587153d1d3bbec0c4e5213185b8adaaded8aa48
adarsh-raghav <aadarshraghav@gmail.com>
2022-01-29 12:39:48 +0530

    For 1080x1920

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
 import React, { useState } from 'react';
 import logo from './assets/bf.jpg'
-import logo1 from './assets/VS.jpg'
-import bg from './assets/cold-bg.jpeg'
 const api = {
-  key: "91038c2e26aa8f9c4857f9a7fa83d964",
+  key: "",
   base: "https://api.openweathermap.org/data/2.5/",
+  lat: "28.426488993383046",
+  lon:"77.03148632322328"
 }
 function App() {
+
   const[query, setQuery] = useState('');
   const[weather, setWeather] = useState({});
 
-  const aqisearch = (evt) =>{
-    if(evt.key == "Enter"){
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-        .then(res=>res.json())
-        .then(result=>{
-          setWeather(result);
-          setQuery('');
-          console.log(result);
-        })
-    }
-  }
-
   const search = evt =>{
     if(evt.key == "Enter"){
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      fetch(`${api.base}air_pollution?lat=${api.lat}&lon=${api.lon}&APPID=${api.key}`)
         .then(res=>res.json())
         .then(result=>{
           setWeather(result);
           setQuery('');
-          console.log(result);
+          console.log(result)
         })
     }
   }
@@ -49,8 +38,7 @@ function App() {
   return (
     <div className="app">
       <main>
-      <div className='bg'>
-          <img src={bg} alt="Background" className='Background' />
+      <div className='color'>
         <div className="search-box">
           <input 
           type="text"
@@ -63,27 +51,33 @@ function App() {
         </div>
 
         <img src={logo} alt="Logo" className='Logo' />
-        <img src={logo1} alt="Logo1" className='Logo1' />
 
         
-        {(typeof weather.main != "undefined") ? (
+        {(typeof weather.coord != "undefined") ? (
         <div>
-
           <div className='location-box'>
             <div className='location'>
-              {weather.name}, 
-              {weather.sys.country}
+              Candor TechSpace, HR
             </div>
             <div className='date'>{dateBuilder(new Date())}</div>
           </div>
 
           <div className='weather-box'>
-            <div className={(Math.round(weather.main.temp))>10? "hightemp": "temp"}>
-              {Math.round(weather.main.temp)}
-            </div>
-            <div className='weather'>
-              {weather.weather[0].main}
-            </div>
+            <div className={(Math.round(weather.list[0].main.aqi))>10? "hightemp": "temp"}>
+              {(weather.list[0].components.pm10)}
+              <div className='aqilabel'>
+                Inside AQI
+              </div>
+            </div> 
+          </div>
+
+          <div className='pm2box'>
+            <div className="hightemp">
+              {(weather.list[0].components.pm2_5)}
+              <div className='aqilabel'>
+                OutSide AQI
+              </div>
+            </div> 
           </div>
 
         </div>
